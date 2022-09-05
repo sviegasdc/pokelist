3422593fcd5538b30821c20d88317f029fe84555
S <77491046+sviegasdc@users.noreply.github.com>
2022-09-05 14:14:44 -0300

    listagem de pokemons

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-function pegandoURL(){
-    let url = "https://pokeapi.co/api/v2/pokemon/151";
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            document.getElementById('nome').innerHTML = data ['name'];
-            document.getElementById('id_pokemon').innerHTML = data ['id'];
-            let img = data['sprites']['front_default'];
-            document.getElementById('pic').setAttribute("src",img);
-        })
-        .catch((erro) => {
-            console.log("ERRO" + erro);
-        });
-        
-}
 
-pegandoURL()
+const TemosquePegar = () => {
+    for (let i = 1; i <= 151; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const pokemon = {};
+                pokemon['nome'] = data.name;
+                pokemon['id'] = data.id;
+                pokemon['pic'] = data.sprites['front_default'];
+
+                const id = document.createElement(`p`);
+                const texto = document.createElement(`h2`);
+                const foto = document.createElement(`img`);
+
+                foto.setAttribute("class", "sprites");
+                foto.setAttribute("src", `${pokemon.pic}`);
+
+                id.setAttribute("class", "pokemon_id");
+                texto.setAttribute("class", "pokemon_name");
+
+                texto.innerHTML = `${pokemon.nome}`;
+                id.innerHTML = `${pokemon.id}`;
+                document.getElementById("lista").appendChild(id);
+                document.getElementById("lista").appendChild(texto);
+                document.getElementById("lista").appendChild(foto);
+
+            })
+            .catch((erro) => {
+                console.log("ERRO" + erro);
+            });
+    }
+};
+
+TemosquePegar()
